0ba2a9518d00d528ad8698d81af6b895f113a95c
Antony Cabeza <antonycabeza@gmail.com>
2020-09-20 18:28:26 -0400

    Colocando animaciones

--- js/app.js
+++ js/app.js
@@ -18,18 +18,32 @@ const game = () => {
         const options = document.querySelectorAll('.options button');
         const playerHand = document.querySelector('.player-hand');
         const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll('.hands img');
+
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function(){
+                this.style.animation = "";
+            })
+        })
         //Computer Options
         const computerOptions = ['rock', 'paper', 'scissors'];
         options.forEach(option => {
             option.addEventListener('click', function() {
+                playerHand.src = './assets/rock.png'
+                computerHand.src = './assets/rock.png'
                 //Computer Choice
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[computerNumber];
-                //Here is where we call compare hands
-                compareHands(this.textContent, computerChoice);
-                //Update images
-                playerHand.src = `./assets/${this.textContent}.png`;
-                computerHand.src = `./assets/${computerChoice}.png`;
+                setTimeout(()=>{
+                    //Here is where we call compare hands
+                    compareHands(this.textContent, computerChoice);
+                    //Update images
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;
+                }, 2000)
+                //Animation
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease"
             });
         });
     };
